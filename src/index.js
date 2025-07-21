147d6f888040d00d5159420ec699012609641855
binit <binitgupta.1711@gmail.com>
2025-07-21 18:43:41 +0530

    Add weather API functionality and fetchWeather function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
 import './searchInput.css';
+import './weatherAPI.js';
 
 console.log('Hello World!');
---
+++ src/weatherAPI.js
@@ -0,0 +1,8 @@
+async function fetchWeather(){
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/?key=VB6TWKC9V7ZZHGW9RJ6J83C67", {mode: "cors"} )
+    const result = await response.json()
+    console.log(result)
+}
+fetchWeather()
+
+export {fetchWeather}
