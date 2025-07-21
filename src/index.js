427f5a3e07dae137806bddc765fa0360973ca898
binit <binitgupta.1711@gmail.com>
2025-07-21 21:06:39 +0530

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import './searchInput.css';
-import { handleSearchInput } from './searchInput.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  handleSearchInput();
-  console.log('App initialized');
-});
